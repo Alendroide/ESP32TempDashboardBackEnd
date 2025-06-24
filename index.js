7e69881a48e713b28b6a9f625b6e6d469401afd9
Alendroide <alendroideyt@gmail.com>
2025-06-23 19:58:02 -0500

    Backend conectado a MQTT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const mqtt = require('mqtt');
 const express = require("express");
 const app = express();
 
@@ -13,6 +14,44 @@ app.use(express.json());
 
 const tempsRouter = require('./src/routers/temp.router');
 app.use('/temperatures',tempsRouter);
+
+const brokerURL = "ws://broker.hivemq.com:8000/mqtt";
+const client = mqtt.connect(brokerURL);
+
+const tempTopic = "pepe/esp32/temperatura";
+const gasTopic = "pepe/esp32/aire";
+
+client.on('connect', () => {
+  console.log('✅ Conectado al broker MQTT');
+  client.subscribe(tempTopic, (err) => {
+    if (err) {
+      console.error('❌ Error al suscribirse:', err);
+    } else {
+      console.log(`📡 Suscrito al tópico: ${tempTopic}`);
+    }
+  });
+  client.subscribe(gasTopic, (err) => {
+    if (err) {
+      console.error('❌ Error al suscribirse:', err);
+    } else {
+      console.log(`📡 Suscrito al tópico: ${gasTopic}`);
+    }
+  });
+});
+
+client.on('message', (receivedTopic, message) => {
+    const payload = message.toString();
+    console.log(`📥 Mensaje recibido en ${receivedTopic}: ${payload}`);
+    
+});
+
+client.on('error', (err) => {
+  console.error('🚨 Error en MQTT:', err);
+});
+
+client.on('close', () => {
+  console.log('📴 Conexión MQTT cerrada');
+});
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
